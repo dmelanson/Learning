3ddea289f1de23eef58b354cbbaad34813538815
dmelanson <dennis@collectivepoint.com>
2017-02-28 17:11:21 -0500

    Graph app working

---
+++ 2017/AmazonGraphApp/AmazonGraphAppLib/BitmapGrouper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AmazonGraphAppLib
+{
+
+
+    public class BitmapGrouper
+    {
+        private HashSet<Tuple<int, int>> Visited = new HashSet<Tuple<int, int>>();
+        int count;
+
+
+        public void CheckBit(int[][] ar, int x, int y, bool newGroup)
+        {
+            if (Visited.Contains(new Tuple<int, int>(x, y)))
+                return;
+
+            Visited.Add(new Tuple<int, int>(x, y));
+
+            if (ar[x][y] == 0)
+                return;
+
+            if (newGroup)
+                count++;
+
+            var neighbors = AdjacentElements<int>(ar, x, y).ToArray();
+
+            foreach(var neighbor in neighbors)
+            {
+                CheckBit(ar, neighbor.Item1, neighbor.Item2, false);
+            }
+        }
+
+        public static IEnumerable<Tuple<int, int>> AdjacentElements<T>(T[][] arr, int row, int column)
+        {
+            int rows = arr.Length;
+            int columns = arr[0].Length;
+
+            for (int j = row - 1; j <= row + 1; j++)
+                for (int i = column - 1; i <= column + 1; i++)
+                    if (i >= 0 && j >= 0 && i < columns && j < rows && !(j == row && i == column))
+                        yield return new Tuple<int, int> (j, i);
+        }
+
+
+        public int GetGroupCountFromBitmap(int[][] ar)
+        {
+            var n = ar.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                var width = ar[i].Length;
+                for (int j = 0; j < width; j++)
+                {
+                    CheckBit(ar, i,j, true);
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 2017/AmazonGraphApp/AmazonGraphAppTest/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AmazonGraphAppLib;
+
+namespace AmazonGraphAppTest
+{
+    [TestClass]
+    public class when_group_count_2
+    {
+        [TestMethod]
+        public void should_return_2()
+        {
+            var ar = new int[][] {  new int[]{1, 1, 0, 0, 0},
+                                    new int[]{0, 0, 0, 0, 1},
+                                    new int[]{1, 0, 0, 1, 1},
+                                    new int[]{1, 1, 1, 0, 1},
+                                    new int[]{1, 0, 1, 0, 1} };
+
+            var grouper = new BitmapGrouper();
+            var cnt = grouper.GetGroupCountFromBitmap(ar);
+
+            Assert.AreEqual<int>(2, cnt);
+        }
+    }
+
+    [TestClass]
+    public class when_group_count_3
+    {
+        [TestMethod]
+        public void should_return_3()
+        {
+            var ar = new int[][] {  new int[]{0, 0, 1, 0, 0, 1},
+                                    new int[]{1, 0, 1, 0, 1, 1},
+                                    new int[]{1, 1, 1, 0, 0, 1},
+                                    new int[]{0, 0, 0, 1, 0, 0},
+                                    new int[]{0, 0, 0, 0, 0, 1} };
+
+            var grouper = new BitmapGrouper();
+            var cnt = grouper.GetGroupCountFromBitmap(ar);
+
+            Assert.AreEqual<int>(3, cnt);
+        }
+    }
+
+    [TestClass]
+    public class when_group_count_1
+    {
+        [TestMethod]
+        public void should_return_1()
+        {
+            var ar = new int[][] {  new int[]{0, 0, 1, 0, 0, 1},
+                                    new int[]{1, 0, 1, 0, 1, 1},
+                                    new int[]{1, 1, 1, 1, 0, 1},
+                                    new int[]{0, 0, 0, 1, 0, 0},
+                                    new int[]{0, 0, 0, 0, 1, 1} };
+
+            var grouper = new BitmapGrouper();
+            var cnt = grouper.GetGroupCountFromBitmap(ar);
+
+            Assert.AreEqual<int>(1, cnt);
+        }
+    }
+
+    [TestClass]
+    public class when_group_count_4
+    {
+        [TestMethod]
+        public void should_return_4()
+        {
+            var ar = new int[][] {  new int[]{0, 0, 1, 0, 0, 1},
+                                    new int[]{1, 0, 1, 0, 0, 0},
+                                    new int[]{1, 0, 1, 1, 0, 1},
+                                    new int[]{0, 0, 0, 1, 0, 0},
+                                    new int[]{0, 0, 0, 0, 1, 1} };
+
+            var grouper = new BitmapGrouper();
+            var cnt = grouper.GetGroupCountFromBitmap(ar);
+
+            Assert.AreEqual<int>(4, cnt);
+        }
+    }
+
+
+    [TestClass]
+    public class when_group_count_5
+    {
+        [TestMethod]
+        public void should_return_5()
+        {
+            var ar = new int[][] {  new int[]{0, 0, 1, 0, 0, 1, 0, 0},
+                                    new int[]{1, 0, 1, 0, 0, 1, 0, 0},
+                                    new int[]{1, 0, 1, 1, 0, 1, 0, 1},
+                                    new int[]{0, 0, 0, 1, 0, 0, 0, 0},
+                                    new int[]{0, 0, 0, 1, 0, 1, 1, 1},
+                                    new int[]{0, 0, 0, 1, 0, 0, 0, 1},
+                                    new int[]{0, 0, 0, 0, 1, 1, 0, 1} };
+
+            var grouper = new BitmapGrouper();
+            var cnt = grouper.GetGroupCountFromBitmap(ar);
+
+            Assert.AreEqual<int>(5, cnt);
+        }
+    }
+
+}
