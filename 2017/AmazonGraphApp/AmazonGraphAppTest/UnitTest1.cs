b697840b4e366d384125a86bbe4c5f87403eb0ec
dmelanson <dennis@collectivepoint.com>
2017-02-28 17:14:38 -0500

    Change test

--- 2017/AmazonGraphApp/AmazonGraphAppTest/UnitTest1.cs
+++ 2017/AmazonGraphApp/AmazonGraphAppTest/UnitTest1.cs
@@ -50,7 +50,9 @@ namespace AmazonGraphAppTest
         {
             var ar = new int[][] {  new int[]{0, 0, 1, 0, 0, 1},
                                     new int[]{1, 0, 1, 0, 1, 1},
+                                    new int[]{1, 0, 1, 0, 0, 1},
                                     new int[]{1, 1, 1, 1, 0, 1},
+                                    new int[]{0, 0, 0, 1, 1, 0},
                                     new int[]{0, 0, 0, 1, 0, 0},
                                     new int[]{0, 0, 0, 0, 1, 1} };
 
